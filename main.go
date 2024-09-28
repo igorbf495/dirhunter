9f96af0c250a7a83902404d977c7e5cf67fe02e9
igorbf495 <igorcarvalhofelipe495@gmail.com>
2024-09-28 18:37:39 -0300

    feat: proxy

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 	"sync"
@@ -19,19 +20,21 @@ func main() {
 	baseURL := flag.String("url", "", "URL alvo para força bruta (ex: http://example.com)")
 	wordlistFile := flag.String("wordlist", "", "Caminho para o arquivo de wordlist (ex: wordlist.txt)")
 	threads := flag.Int("threads", 10, "Número de threads (padrão: 10)")
+	proxy := flag.String("proxy", "", "URL do proxy (ex: http://127.0.0.1:8080)")
 
 	flag.Usage = func() {
 		fmt.Println(`DirHunter - Força bruta de diretórios em servidores web
 Uso:
-  dirhunter -url <URL alvo> -wordlist <arquivo de wordlist> [-threads <número de threads>]
+  dirhunter -url <URL alvo> -wordlist <arquivo de wordlist> [-threads <número de threads>] [-proxy <URL do proxy>]
 
 Parâmetros:
   -url         URL alvo para força bruta (ex: http://example.com)
   -wordlist    Caminho para o arquivo de wordlist (ex: wordlist.txt)
   -threads     Número de threads (opcional, padrão: 10)
+  -proxy       URL do proxy (opcional)
 
 Exemplo:
-  dirhunter -url http://example.com -wordlist wordlist.txt -threads 20
+  dirhunter -url http://example.com -wordlist wordlist.txt -threads 20 -proxy http://127.0.0.1:8080
 `)
 	}
 
@@ -51,8 +54,7 @@ Exemplo:
 		return
 	}
 
-	startBruteForce(*baseURL, words, *threads)
-	
+	startBruteForce(*baseURL, words, *threads, *proxy)
 }
 
 func printSkull() {
@@ -83,14 +85,14 @@ func readWordlist(filename string) ([]string, error) {
 	return words, scanner.Err()
 }
 
-func startBruteForce(baseURL string, wordlist []string, threads int) {
+func startBruteForce(baseURL string, wordlist []string, threads int, proxy string) {
 	var wg sync.WaitGroup
 	wordChan := make(chan string)
 	foundCount := 0
 
 	for i := 0; i < threads; i++ {
 		wg.Add(1)
-		go worker(baseURL, wordChan, &wg, &foundCount)
+		go worker(baseURL, wordChan, &wg, &foundCount, proxy)
 	}
 
 	for _, word := range wordlist {
@@ -100,21 +102,33 @@ func startBruteForce(baseURL string, wordlist []string, threads int) {
 	close(wordChan)
 
 	wg.Wait()
+	color.Blue("Busca concluída! Total de URLs encontradas: %d", foundCount)
 }
 
-func worker(baseURL string, wordChan <-chan string, wg *sync.WaitGroup, foundCount *int) {
+func worker(baseURL string, wordChan <-chan string, wg *sync.WaitGroup, foundCount *int, proxy string) {
 	defer wg.Done()
 
 	for word := range wordChan {
 		url := fmt.Sprintf("%s/%s", strings.TrimRight(baseURL, "/"), word)
-		makeRequest(url, foundCount)
+		makeRequest(url, foundCount, proxy)
 	}
 }
 
-func makeRequest(url string, foundCount *int) {
+func makeRequest(url string, foundCount *int, proxy string) {
 	client := &http.Client{
 		Timeout: 10 * time.Second,
 	}
+
+	// Configura o proxy, se fornecido
+	if proxy != "" {
+		proxyURL, err := url.Parse(proxy)
+		if err != nil {
+			color.Red("Erro ao parsear o proxy: %v", err)
+			return
+		}
+		client.Transport = &http.Transport{Proxy: http.ProxyURL(proxyURL)}
+	}
+
 	resp, err := client.Get(url)
 	if err != nil {
 		color.Yellow("Erro ao fazer requisição: %v", err)
